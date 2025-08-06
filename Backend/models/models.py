4b2e91f70d9d4fcb81c22aded36851d95eae34a3
Erik Andres <erikqtrs@gmail.com>
2025-08-06 13:12:10 -0500

    Creacion de modelos de datos
    * UserInput: los datos que enviará el usuario para la recomendacion
    * AmazonProduct: los datos de productos consultados en amazon
    * FinalResponse: datos para respuesta final del servidor

---
+++ Backend/models/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+class UserInput(BaseModel):
+    profession: str = Field(min_length=5)
+    device: str = Field(min_length=5)
+    activity: str = Field(min_length=5)
+
+class AmazonProducts(BaseModel):
+    title: str
+    price: str | None = None
+    link: str
+
+class FinalResponse(BaseModel):
+    components: dict
+    reason: str
+    similar_products: List[AmazonProducts]
